324a70d57bb4aad1b1232eb3cba1e6da9c0133e2
jesse <2912150071@qq.com>
2020-05-17 17:11:56 +0800

    完善readme

--- static/main.go
+++ static/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	address := ":8096"
 	path := "./dist"
+	fmt.Println("Listening and serving HTTP on ", address, "\r\n webroot dir ", path)
 	err := http.ListenAndServe(address, http.FileServer(http.Dir(path)))
 	if err != nil {
 		panic(err)
